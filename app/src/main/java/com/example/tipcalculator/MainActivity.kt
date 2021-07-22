7a8109b1da7a146d253728774f932c1e8606fbff
Charlie Sparks <charlie.sparks@kroger.com>
2021-07-22 17:08:41 -0400

    got all the logic wired up--need to figure out why my toDouble method isn't working

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
+    val AMAZING: String = "AMAZING"
+    val GOOD: String = "GOOD"
+    val OK: String = "OK"
+
+    lateinit var editText: EditText
+    lateinit var amazingRadio: RadioButton
+    lateinit var goodRadio: RadioButton
+    lateinit var okRadio: RadioButton
+
+    var currentlySelectedRadio: String = AMAZING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.cost_of_service)
+        amazingRadio = findViewById(R.id.option_twenty_percent)
+        goodRadio = findViewById(R.id.option_eighteen_percent)
+        okRadio = findViewById(R.id.option_fifteen_percent)
+
+        amazingRadio.setOnClickListener { updateSelectedRadio(AMAZING) }
+        goodRadio.setOnClickListener { updateSelectedRadio(GOOD) }
+        okRadio.setOnClickListener { updateSelectedRadio(OK) }
+    }
+
+    private fun updateSelectedRadio(serviceQuality: String) {
+        currentlySelectedRadio = serviceQuality
+    }
+
+    private fun calculateTotalCost(): Double {
+        var tipAmount: Double
+        val costOfService: Double = editText.text.toString().toDouble()
+
+        if (currentlySelectedRadio === AMAZING) {
+            tipAmount = costOfService * .2
+            return costOfService + tipAmount
+        } else if (currentlySelectedRadio === GOOD) {
+            tipAmount = costOfService * .18
+            return costOfService + tipAmount
+        } else {
+            tipAmount = costOfService * .15
+            return costOfService + tipAmount
+        }
     }
 }
